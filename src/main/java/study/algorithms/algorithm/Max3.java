848dd0b5bd63c0ffd0d0e0872a82c41506d0b06b
JinyWiny <jiny95.dev@gmail.com>
2025-03-05 21:54:26 +0900

    algorithms(basic_algorithm): 세 겂의 최댓값 구하기

---
+++ src/main/java/study/algorithms/algorithm/Max3.java
@@ -0,0 +1,28 @@
+package study.algorithms.algorithm;
+
+import java.util.Scanner;
+
+class Max3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("세 정수의 최댓값");
+
+        System.out.print("a의 값: ");
+        int a = scanner.nextInt();
+
+        System.out.print("b의 값: ");
+        int b = scanner.nextInt();
+
+        System.out.print("c의 값: ");
+        int c = scanner.nextInt();
+
+        int max = a;
+
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        System.out.println("최댓값은 " + max);
+    }
+}
